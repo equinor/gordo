5e387907f16c025bb7b226116385b8a5c9cfc064
Erik Parmann <epar@statoil.com>
2018-10-27 21:23:37 +0200

    Pushing docker images to azure

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
+export DOCKER_REGISTRY := auroradevacr.azurecr.io
 
-MODEL_BUILDER_IMG_NAME := milesg/gordo-components-builder:latest
-MODEL_SERVER_IMG_NAME  := milesg/gordo-components-server:latest
-MODEL_SERVER_BASE_IMG  := milesg/gordo-components-serverbase:latest
+MODEL_BUILDER_IMG_NAME := gordo-components/gordo-model-builder
+MODEL_SERVER_IMG_NAME  := gordo-components/gordo-model-server
+MODEL_SERVER_BASE_IMG  := gordo-components/gordo-serverbase
 
 # Create the image capable to building/training a model
 model-builder:
@@ -13,12 +14,27 @@ model-server:
 	cd ./gordo_components/runtime && s2i build . -e HTTPS_PROXY=$(HTTPS_PROXY) \
 	 $(MODEL_SERVER_BASE_IMG) $(MODEL_SERVER_IMG_NAME)
 
+push-server: model-server
+	export DOCKER_NAME=$(MODEL_SERVER_IMG_NAME);\
+	export DOCKER_IMAGE=$(MODEL_SERVER_IMG_NAME);\
+	./docker_push.sh
+
+push-builder: model-builder
+	export DOCKER_NAME=$(MODEL_BUILDER_IMG_NAME);\
+	export DOCKER_IMAGE=$(MODEL_BUILDER_IMG_NAME);\
+	./docker_push.sh
+
 # Publish images to the currently logged in docker repo
-push-images:
-	docker push $(MODEL_BUILDER_IMG_NAME)
-	docker push $(MODEL_SERVER_IMG_NAME)
+push-dev-images: push-builder push-server
+
+push-prod-images: export GORDO_PROD_MODE:="true"
+push-prod-images:	push-builder push-server
+
+images: model-builder model-server
 
 test:
 	python setup.py test
 
-all: test model-builder model-server push-images
+all: test images push-dev-images
+
+.PHONY: model-builder model-server push-server push-builder push-dev-images push-prod-images images test all
---
+++ docker_push.sh
@@ -0,0 +1,58 @@
+# Script to push an docker image to a docker registry. The docker image can either exist and be
+# provided in the env variable DOCKER_IMAGE, or it can be built if DOCKER_IMAGE is empty and DOCKER_FILE is provided.
+
+# Pushes a docker image with name DOCKER_NAME, and tag tag with the 8 first chars of the git commit,
+# and if the current commit is tagged it also pushes with those tags. So e.g. if git HEAD is commit "3f6f963" and with tag "0.0.2" then
+# two tags are pushed, both "gordo-infrastructure/gordo-deploy:3f6f963" and
+# "gordo-infrastructure/gordo-deploy:0.0.2" if DOCKER_NAME is "gordo-infrastructure/gordo-deploy".
+
+# Expects the following environment variables to be set:
+# DOCKER_USERNAME: Required
+# DOCKER_PASSWORD: Required
+# DOCKER_NAME: Required. Docker name to push to.
+# DOCKER_FILE: Semi-Required. Dockerfile to build. Either DOCKER_IMAGE or DOCKER_FILE must be set.
+# DOCKER_IMAGE: Semi-Required. The local docker image to push. Either DOCKER_IMAGE or DOCKER_FILE must be set.
+# DOCKER_REGISTRY: Docker registry to push to. Defaults to auroradevacr.azurecr.io
+# GORDO_PROD_MODE: If false then pushed tags will include a -dev suffix. Defaults to false
+
+
+export DOCKER_REGISTRY="${DOCKER_REGISTRY:-auroradevacr.azurecr.io}"
+if [[ -z "${DOCKER_PASSWORD}" ]]; then
+    echo "DOCKER_PASSWORD must be set, exiting"
+    exit 1
+fi
+if [[ -z "${DOCKER_USERNAME}" ]]; then
+    echo "DOCKER_USERNAME must be set, exiting"
+    exit 1
+fi
+if [[ -z "${DOCKER_NAME}" ]]; then
+    echo "DOCKER_NAME must be set, exiting"
+    exit 1
+fi
+
+# Logging in to the docker registry
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin $DOCKER_REGISTRY
+export git_sha=$(git rev-parse HEAD | cut -c 1-8)
+
+if [[ -z "${DOCKER_IMAGE}" ]]; then
+    if [[ -z "${DOCKER_FILE}" ]]; then
+        echo "DOCKER_IMAGE or DOCKER_FILE must be provided, exiting"
+        exit 1
+    fi
+    docker build -t $git_sha  -f $DOCKER_FILE .
+    export DOCKER_IMAGE=$git_sha
+fi
+
+if [[ -z "${GORDO_PROD_MODE}" ]]; then
+    export suffix="-dev"
+else
+    export suffix=""
+fi
+
+docker tag $DOCKER_IMAGE $DOCKER_REGISTRY/$DOCKER_NAME:$git_sha$suffix
+docker push $DOCKER_REGISTRY/$DOCKER_NAME:$git_sha$suffix
+
+git tag --points-at HEAD | while read -r tag ; do
+    docker tag $DOCKER_IMAGE $DOCKER_REGISTRY/$DOCKER_NAME:$tag$suffix
+    docker push $DOCKER_REGISTRY/$DOCKER_NAME:$tag$suffix
+done
