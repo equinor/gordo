121293df012de224bc438360daea1a535de56a19
Kristian Flikka <kristian.flikka@gmail.com>
2019-01-21 08:08:02 +0100

    Make formatting test case running path insensitive

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -10,14 +10,17 @@ class FormattingTestCase(unittest.TestCase):
         """
         Test existing code would require no re-formatting
         """
+        project_path = os.path.join(os.path.dirname(__file__), "..")
+        gordo_components_path = os.path.join(project_path, "gordo_components")
+        tests_path = os.path.join(project_path, "tests")
         runner = CliRunner()
         resp = runner.invoke(
             black.main,
             [
                 "--check",
                 "-v",
-                "gordo_components",
-                "tests",
+                gordo_components_path,
+                tests_path,
                 "--exclude",
                 r".*_version.py",
             ],
