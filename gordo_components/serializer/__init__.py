7774f7aa86567dcd44d70c730030866b826b7363
Miles Granger <miles59923@gmail.com>
2019-01-24 10:41:05 +0100

    Support dumps and loads for gordo serializer

--- gordo_components/serializer/__init__.py
+++ gordo_components/serializer/__init__.py
@@ -1,3 +1,3 @@
 from .pipeline_from_definition import pipeline_from_definition
 from .pipeline_into_definition import pipeline_into_definition
-from .serializer import dump, load, load_metadata
+from .serializer import dump, dumps, load, loads, load_metadata
