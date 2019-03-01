11601354a6fa9a690b01c3436814a15413217a56
Miles Granger <miles59923@gmail.com>
2019-03-01 09:38:59 +0100

    Add .score(...) abstract method to GordoBase

--- gordo_components/model/base.py
+++ gordo_components/model/base.py
@@ -1,22 +1,41 @@
 # -*- coding: utf-8 -*-
 
 import abc
+from typing import Optional, Union
+
+import numpy as np
+import pandas as pd
 
 
 class GordoBase(abc.ABC):
-
     @abc.abstractmethod
     def __init__(self, **kwargs):
         """Initialize the model"""
+        ...
 
     @abc.abstractmethod
     def get_params(self, deep=False):
         """Return a dict containing all parameters used to initialized object"""
+        ...
 
     @abc.abstractclassmethod
     def load_from_dir(cls, directory: str):
         """Load this model from a directory"""
+        ...
 
     @abc.abstractmethod
     def save_to_dir(self, directory: str):
         """Save this model to a directory"""
+        ...
+
+    @abc.abstractmethod
+    def score(
+        self,
+        X: Union[np.ndarray, pd.DataFrame],
+        y: Optional[Union[np.ndarray, pd.DataFrame]] = None,
+        sample_weight: Optional[np.ndarray] = None,
+    ):
+        """
+        Score the model; must implement the correct default scorer based on model type
+        """
+        ...
