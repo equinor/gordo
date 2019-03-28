99ff053b30e0b7c364ceb9a4883134f60953b1d8
Miles Granger <miles59923@gmail.com>
2019-03-28 13:22:44 +0100

    Support .to_dict() for all DataProviders

--- tests/test_data_providers.py
+++ tests/test_data_providers.py
@@ -1,11 +1,14 @@
 import re
 import unittest
+import json
 from datetime import datetime
 from typing import Iterable, List, Pattern, Any
 
 import pandas as pd
+import pytest
 
 from gordo_components.data_provider.base import GordoBaseDataProvider
+from gordo_components.data_provider import providers
 from gordo_components.data_provider.providers import (
     load_dataframes_from_multiple_providers,
 )
@@ -82,3 +85,37 @@ class LoadMultipleDataFramesTest(unittest.TestCase):
         )
         self.assertEqual(dfs[0].columns[0], "ab.*")
         self.assertEqual(dfs[1].columns[0], ".*b.*")
+
+
+@pytest.mark.parametrize(
+    "provider,expected_params",
+    (
+        (
+            providers.RandomDataProvider(200, max_size=205),
+            {"min_size": 200, "max_size": 205},
+        ),
+        (
+            providers.InfluxDataProvider("measurement", value_name="Value"),
+            {"measurement": "measurement", "value_name": "Value"},
+        ),
+    ),
+)
+def test_data_provider_serializations(
+    provider: GordoBaseDataProvider, expected_params: dict
+):
+    """
+    Test a given provider can be serialized to dict and back
+    """
+
+    encoded = provider.to_dict()
+
+    # Verify the expected parameter kwargs match
+    for k, v in expected_params.items():
+        assert encoded[k] == v
+
+    # Should have inserted the name of the class as 'type'
+    assert provider.__class__.__name__ == encoded.pop("type")
+
+    # Should be able to recreate the object from encoded directly
+    cloned = provider.__class__(**encoded)
+    assert type(cloned) == type(provider)
