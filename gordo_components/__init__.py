419d67b5ce12e764236f43b780fd407231f99c03
Miles Granger <miles59923@gmail.com>
2019-04-12 12:36:31 +0200

    Change dev versioning to all dot seperators

--- gordo_components/__init__.py
+++ gordo_components/__init__.py
@@ -1,4 +1,13 @@
+from contextlib import redirect_stderr
+import os
+
 try:
     from ._version import version as __version__
+
+    __version__ = __version__.replace("+", ".")
 except ImportError:
     __version__ = "0.0.0"
+
+# Hide the abundantly annoying 'Using X backend' from kears
+with redirect_stderr(open(os.devnull, "w")):
+    import keras  # noqa
