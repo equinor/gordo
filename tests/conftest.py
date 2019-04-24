1a63f9aada097a98e709925aaa34979aa6f8af5b
Miles Granger <miles59923@gmail.com>
2019-04-24 15:21:03 +0200

    Refactor test_client to use influx pytest.fixture & pytests

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,9 @@ import pytest
 import numpy as np
 
 from gordo_components import serializer
-from tests.gordo_components.server.test_gordo_server import SENSORS
+from tests.gordo_components.server.test_gordo_server import SENSORS, influxdatabase
+from tests.utils import watchman
+
 
 logger = logging.getLogger(__name__)
 
@@ -36,32 +38,66 @@ def tmp_dir():
     return tempfile.TemporaryDirectory()
 
 
-@pytest.fixture
-def trained_model_directory(tmp_dir, sensors: typing.List[str]):
+@pytest.yield_fixture(scope="session")
+def trained_model_directory(sensors: typing.List[str]):
     """
     Fixture: Train a basic AutoEncoder and save it to a given directory
     will also save some metadata with the model
     """
-    definition = ruamel.yaml.load(
-        """
-        sklearn.pipeline.Pipeline:
-            steps:
-                - sklearn.preprocessing.data.MinMaxScaler
-                - gordo_components.model.models.KerasAutoEncoder:
-                    kind: feedforward_hourglass
-            memory:
-        """,
-        Loader=ruamel.yaml.Loader,
-    )
-    model = serializer.pipeline_from_definition(definition)
-    X = np.random.random(size=len(sensors) * 10).reshape(10, len(sensors))
-    model.fit(X, X)
-    serializer.dump(
-        model,
-        tmp_dir.name,
-        metadata={
-            "dataset": {"tag_list": sensors, "resolution": "10T"},
-            "user-defined": {"model-name": "test-model", "machine-name": "machine-1"},
-        },
-    )
-    return tmp_dir.name
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        definition = ruamel.yaml.load(
+            """
+            sklearn.pipeline.Pipeline:
+                steps:
+                    - sklearn.preprocessing.data.MinMaxScaler
+                    - gordo_components.model.models.KerasAutoEncoder:
+                        kind: feedforward_hourglass
+                memory:
+            """,
+            Loader=ruamel.yaml.Loader,
+        )
+        model = serializer.pipeline_from_definition(definition)
+        X = np.random.random(size=len(sensors) * 10).reshape(10, len(sensors))
+        model.fit(X, X)
+        serializer.dump(
+            model,
+            tmp_dir,
+            metadata={
+                "dataset": {"tag_list": sensors, "resolution": "10T"},
+                "user-defined": {
+                    "model-name": "test-model",
+                    "machine-name": "machine-1",
+                },
+            },
+        )
+        yield tmp_dir
+
+
+@pytest.yield_fixture(scope="module")
+def influxdb(
+    sensors, db_name="testdb", user="root", password="root", measurement="sensors"
+):
+    with influxdatabase(
+        sensors=sensors,
+        db_name=db_name,
+        user=user,
+        password=password,
+        measurement=measurement,
+    ):
+        yield
+
+
+@pytest.yield_fixture(scope="module")
+def watchman_service(
+    trained_model_directory,
+    host="localhost",
+    project="gordo-test",
+    targets=["machine-1"],
+):
+    with watchman(
+        host=host,
+        project=project,
+        targets=targets,
+        model_location=trained_model_directory,
+    ):
+        yield
