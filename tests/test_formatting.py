774f7e7d9a39dd16f0c5884b145d19a944730fa4
Kristian Flikka <kristian.flikka@gmail.com>
2019-05-03 12:31:29 +0200

    Change running of black tests to use os.system, update black

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -1,32 +1,24 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 import unittest
-import black
-from click.testing import CliRunner
 
 
 class FormattingTestCase(unittest.TestCase):
-    def test_project_formatting(self):
-        """
-        Test existing code would require no re-formatting
-        """
+    def test_formatting_black(self):
         project_path = os.path.join(os.path.dirname(__file__), "..")
         gordo_components_path = os.path.join(project_path, "gordo_components")
         tests_path = os.path.join(project_path, "tests")
-        runner = CliRunner()
-        resp = runner.invoke(
-            black.main,
-            [
-                "--check",
-                "-v",
-                gordo_components_path,
-                tests_path,
-                "--exclude",
-                r".*_version.py",
-            ],
-        )
-        self.assertEqual(
-            resp.exit_code,
-            0,
-            msg=f"Black would still reformat one or ore files:\n{resp.output} \n{resp.exc_info}",
-        )
+        cmd = [
+            sys.executable,
+            "-m",
+            "black",
+            "--check",
+            "-v",
+            gordo_components_path,
+            tests_path,
+            "--exclude",
+            r".*_version.py",
+        ]
+        exit_code = os.system(" ".join(cmd))
+        self.assertEqual(exit_code, 0)
