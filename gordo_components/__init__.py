2fd71d6f8536173f34ae92f1936e768e0118094f
Miles Granger <miles59923@gmail.com>
2019-06-04 11:08:26 +0200

    Add version parser to assign major, minor and patch

--- gordo_components/__init__.py
+++ gordo_components/__init__.py
@@ -1,4 +1,27 @@
+from typing import Tuple
+
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "0.0.0"
+
+
+def _parse_version(version: str) -> Tuple[int, ...]:
+    """
+    Takes a string which starts with standard major.minor.patch.
+    and returns the split of major minor and path as integers
+
+    Parameters
+    ----------
+    version: str
+        The semantic version string
+
+    Returns
+    -------
+    Tuple[int, int, int]
+        major, minor and patch versions
+    """
+    return tuple(int(i) for i in version.split(".")[:3])
+
+
+MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION = _parse_version(__version__)
