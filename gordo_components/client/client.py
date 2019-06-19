05a8cf553cdb4641560e6f285c39797026fe5b99
Erik Parmann <epar@equinor.com>
2019-06-19 22:45:36 +0200

    Client: retry predictions after aiohttp errors

--- gordo_components/client/client.py
+++ gordo_components/client/client.py
@@ -387,7 +387,13 @@ class Client:
                 )
 
             # If it was an IO or TimeoutError, we can retry
-            except (IOError, TimeoutError, FutureTimeoutError, BadRequest) as exc:
+            except (
+                IOError,
+                TimeoutError,
+                FutureTimeoutError,
+                BadRequest,
+                aiohttp.ClientError,
+            ) as exc:
                 if i <= self.n_retries:
                     logger.warning(
                         f"Failed to get response on attempt {i} out of {self.n_retries} attempts."
