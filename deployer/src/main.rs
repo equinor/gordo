4daf8d615abbd15490bc024055b53bcb1cae2f7a
Miles Granger <miles59923@gmail.com>
2019-08-17 13:19:54 +0200

    Switch from Reflector to Informer monitoring

--- deployer/src/main.rs
+++ deployer/src/main.rs
@@ -1,6 +1,8 @@
+use kube::api::Informer;
 use kube::{
+    api::{Api, Object, PostParams, RawApi, Void, WatchEvent},
     client::APIClient,
-    config, api::{Api, Object, PostParams, RawApi, Reflector, Void},
+    config,
 };
 use log::info;
 use serde::{Deserialize, Serialize};
@@ -24,51 +26,56 @@ fn main() -> Result<(), failure::Error> {
     let namespace = std::env::var("NAMESPACE").unwrap_or("kubeflow".into());
 
     let resource = RawApi::customResource("gordo").within(&namespace);
-    let reflector: Reflector<Gordo> = Reflector::raw(client.clone(), resource).init()?;
+    let informer: Informer<Gordo> = Informer::raw(client.clone(), resource).init()?;
 
     loop {
         // Update state changes
-        reflector.poll()?;
+        informer.poll()?;
 
-        // Read updates
-        reflector.read()?
-            .into_iter()
-            .for_each(|crd: Gordo| {
-
-                let jobs = Api::v1Job(client.clone())
-                    .within(&namespace);
-
-                // Create the job.
-                let spec = json!({
-                    "apiVersion": "batch/v1",
-                    "kind": "Job",
-                    "metadata": {
-                        "name": "gordo-deploy-job"
-                    },
-                    "template": {
+        while let Some(event) = informer.pop() {
+            match event {
+                WatchEvent::Added(gordo) => {
+                    // Create the job.
+                    let spec = json!({
+                        "apiVersion": "batch/v1",
+                        "kind": "Job",
                         "metadata": {
-                            "name": "gordo-deploy"
+                            "name": "gordo-deploy-job"
                         },
-                        "spec": {
-                            "containers": [{
-                                "name": "gordo-deploy",
-                                "image": &format!("gordo-infrastructure/gordo-deploy:{}", crd.spec.version),
-                                "env": [
-                                    {"name": "GORDO_CONFIG", "value": &crd.spec.info}
-                                ]
-                            }],
-                            "restartPolicy": "Never"
+                        "template": {
+                            "metadata": {
+                                "name": "gordo-deploy"
+                            },
+                            "spec": {
+                                "containers": [{
+                                    "name": "gordo-deploy",
+                                    "image": &format!("gordo-infrastructure/gordo-deploy:{}", gordo.spec.version),
+                                    "env": [
+                                        {"name": "GORDO_CONFIG", "value": &gordo.spec.info}
+                                    ]
+                                }],
+                                "restartPolicy": "Never"
+                            }
                         }
-                    }
-                });
+                    });
 
-                let gdj_serialized = serde_json::to_vec(&spec).unwrap();
-                let postparams = PostParams::default();
+                    let gdj_serialized = serde_json::to_vec(&spec).unwrap();
+                    let postparams = PostParams::default();
 
-                // Send off job, later we can add support to watching the job if needed via `jobs.watch(..)`
-                info!("Launching job!");
-                jobs.create(&postparams, gdj_serialized).expect("Failed to launch job.");
-
-            });
+                    // Send off job, later we can add support to watching the job if needed via `jobs.watch(..)`
+                    info!("Launching job!");
+                    let jobs = Api::v1Job(client.clone()).within(&namespace);
+                    jobs.create(&postparams, gdj_serialized)
+                        .expect("Failed to launch job.");
+                }
+                WatchEvent::Modified(gordo) => {
+                    info!("Gordo resource modified: {:?}", gordo.metadata.name)
+                }
+                WatchEvent::Deleted(gordo) => {
+                    info!("Gordo resource deleted: {:?}", gordo.metadata.name)
+                }
+                WatchEvent::Error(e) => info!("Gordo resource error: {:?}", e),
+            }
+        }
     }
 }
