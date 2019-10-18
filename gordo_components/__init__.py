287796d6d516ccf404ce43008e3fa1a2a599e3c1
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2019-10-18 13:17:34 +0200

    Moved logging config to gordo_components/__init__.py

--- gordo_components/__init__.py
+++ gordo_components/__init__.py
@@ -1,4 +1,8 @@
+import logging
+import os
+import traceback
 from typing import Tuple
+import warnings
 
 try:
     from ._version import version as __version__
@@ -25,3 +29,37 @@ def _parse_version(version: str) -> Tuple[int, ...]:
 
 
 MAJOR_VERSION, MINOR_VERSION = _parse_version(__version__)
+
+try:
+    # FIXME(https://github.com/abseil/abseil-py/issues/99)
+    # FIXME(https://github.com/abseil/abseil-py/issues/102)
+    # Unfortunately, many libraries that include absl (including Tensorflow)
+    # will get bitten by double-logging due to absl's incorrect use of
+    # the python logging library:
+    #   2019-07-19 23:47:38,829 my_logger   779 : test
+    #   I0719 23:47:38.829330 139904865122112 foo.py:63] test
+    #   2019-07-19 23:47:38,829 my_logger   779 : test
+    #   I0719 23:47:38.829469 139904865122112 foo.py:63] test
+    # The code below fixes this double-logging.  FMI see:
+    #   https://github.com/tensorflow/tensorflow/issues/26691#issuecomment-500369493
+
+    import absl.logging
+
+    logging.root.removeHandler(absl.logging._absl_handler)
+    absl.logging._warn_preinit_stderr = False
+
+except Exception:
+    warnings.warn(f"Failed to fix absl logging bug {traceback.format_exc()}")
+    pass
+
+
+# Set log level, defaulting to DEBUG
+log_level = os.getenv("LOG_LEVEL", "DEBUG").upper()
+azure_log_level = os.getenv("AZURE_DATALAKE_LOG_LEVEL", "INFO").upper()
+
+logging.basicConfig(
+    level=getattr(logging, log_level),
+    format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+)
+
+logging.getLogger("azure.datalake").setLevel(azure_log_level)
