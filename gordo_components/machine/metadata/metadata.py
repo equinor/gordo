de7c2702b758bafb32785dab99ef4bcec3b3e616
Miles Granger <miles59923@gmail.com>
2020-01-06 15:19:39 +0100

    Use fields when needed and default_factory for other dataclasses

--- gordo_components/machine/metadata/metadata.py
+++ gordo_components/machine/metadata/metadata.py
@@ -11,32 +11,32 @@ __all__ = ["Metadata", "BuildMetadata", "ModelBuildMetadata", "CrossValidationMe
 @dataclass
 class CrossValidationMetaData:
     scores: Dict[str, Any] = field(default_factory=dict)
-    cv_duration_sec: Optional[float] = field(default=None)
+    cv_duration_sec: Optional[float] = None
     splits: Dict[str, Any] = field(default_factory=dict)
 
 
 @dataclass_json
 @dataclass
 class ModelBuildMetadata:
-    model_offset: int = field(default=0)
-    model_creation_date: Optional[str] = field(default=None)
-    model_builder_version: str = field(default=__version__)
-    data_query_duration_sec: Optional[float] = field(default=None)
+    model_offset: int = 0
+    model_creation_date: Optional[str] = None
+    model_builder_version: str = __version__
+    data_query_duration_sec: Optional[float] = None
     cross_validation: CrossValidationMetaData = field(
         default_factory=CrossValidationMetaData
     )
-    model_training_duration_sec: Optional[float] = field(default=None)
+    model_training_duration_sec: Optional[float] = None
     model_meta: Dict[str, Any] = field(default_factory=dict)
 
 
 @dataclass_json
 @dataclass
 class BuildMetadata:
-    model: ModelBuildMetadata = ModelBuildMetadata()
+    model: ModelBuildMetadata = field(default_factory=ModelBuildMetadata)
 
 
 @dataclass_json
 @dataclass
 class Metadata:
     user_defined: Dict[str, Any] = field(default_factory=dict)
-    build_metadata: BuildMetadata = BuildMetadata()
+    build_metadata: BuildMetadata = field(default_factory=BuildMetadata)
