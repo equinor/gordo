9e3e511067910540b1ba4dd1c47c5974e5acb2ec
Miles Granger <miles59923@gmail.com>
2020-02-03 14:57:56 +0100

    Add 'dataset' key for build metadata (#902)

--- gordo/machine/metadata/metadata.py
+++ gordo/machine/metadata/metadata.py
@@ -4,7 +4,13 @@ from dataclasses_json import dataclass_json
 from gordo import __version__
 
 
-__all__ = ["Metadata", "BuildMetadata", "ModelBuildMetadata", "CrossValidationMetaData"]
+__all__ = [
+    "Metadata",
+    "BuildMetadata",
+    "ModelBuildMetadata",
+    "CrossValidationMetaData",
+    "DatasetBuildMetadata",
+]
 
 
 @dataclass_json
@@ -21,7 +27,6 @@ class ModelBuildMetadata:
     model_offset: int = 0
     model_creation_date: Optional[str] = None
     model_builder_version: str = __version__
-    data_query_duration_sec: Optional[float] = None
     cross_validation: CrossValidationMetaData = field(
         default_factory=CrossValidationMetaData
     )
@@ -31,8 +36,16 @@ class ModelBuildMetadata:
 
 @dataclass_json
 @dataclass
+class DatasetBuildMetadata:
+    query_duration_sec: Optional[float] = None  # How long it took to get the data
+    dataset_meta: Dict[str, Any] = field(default_factory=dict)
+
+
+@dataclass_json
+@dataclass
 class BuildMetadata:
     model: ModelBuildMetadata = field(default_factory=ModelBuildMetadata)
+    dataset: DatasetBuildMetadata = field(default_factory=DatasetBuildMetadata)
 
 
 @dataclass_json
--- tests/gordo/machine/metadata/test_metadata.py
+++ tests/gordo/machine/metadata/test_metadata.py
@@ -13,9 +13,11 @@ def test_metadata_dataclass():
                 model_offset=0,
                 model_creation_date="2016-01-01",
                 model_builder_version="v1",
-                data_query_duration_sec=1.0,
                 cross_validation=m.CrossValidationMetaData(),
-            )
+            ),
+            dataset=m.DatasetBuildMetadata(
+                query_duration_sec=1, dataset_meta=dict(key="value")
+            ),
         ),
     )
     m.Metadata.from_dict(metadata.to_dict())
