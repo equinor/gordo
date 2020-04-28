e664e387e3fca8925d7dcaa0327e1db50104c17e
Serhii Koropets <skoropets@gmail.com>
2020-04-28 18:54:47 +0300

    black

--- tests/gordo/cli/exception_reporter.py
+++ tests/gordo/cli/exception_reporter.py
@@ -2,7 +2,12 @@ import json
 
 from io import StringIO
 
-from gordo.cli.exceptions_reporter import ReportLevel, ExceptionsReporter, DEFAULT_EXIT_CODE
+from gordo.cli.exceptions_reporter import (
+    ReportLevel,
+    ExceptionsReporter,
+    DEFAULT_EXIT_CODE,
+)
+
 
 def test_report_level():
     level = ReportLevel.get_by_name("MESSAGE")
@@ -12,19 +17,24 @@ def test_report_level():
     levels = ReportLevel.get_names()
     assert len(levels) == 3
 
+
 class _Test1Exception(Exception):
     pass
 
+
 class _Test2Exception(Exception):
     pass
+
 
 def test_exceptions_reporter():
     reporter = ExceptionsReporter(((_Test1Exception, 110),))
     assert reporter.exception_exit_code(_Test1Exception()) == 110
     assert reporter.exception_exit_code(_Test2Exception()) == DEFAULT_EXIT_CODE
+
     def get_result(sio):
         value = sio.getvalue()
         return json.loads(value)
+
     report_file = StringIO()
     reporter.report(ReportLevel.MESSAGE, _Test1Exception("Test message"), report_file)
     assert get_result(report_file) == {
@@ -38,5 +48,4 @@ def test_exceptions_reporter():
     }
     report_file = StringIO()
     reporter.report(ReportLevel.EXIT_CODE, _Test1Exception("Test message"), report_file)
-    assert get_result(report_file) == {
-    }
+    assert get_result(report_file) == {}
