482c87c60217f084eceb50ea4b39efd93fa18e4e
Serhii Koropets <skoropets@gmail.com>
2020-05-07 10:12:18 +0300

    Found exceptions ExceptionsReporter depends on their inheritance level

--- gordo/cli/exceptions_reporter.py
+++ gordo/cli/exceptions_reporter.py
@@ -1,8 +1,8 @@
 import json
 import traceback
 
-from typing import Tuple, Iterable, Type, IO, Optional
-from collections import OrderedDict
+from typing import Tuple, Iterable, Type, IO, Optional, List
+from collections import Counter
 from enum import Enum
 
 from gordo.util import cuts_all_non_ascii_chars
@@ -36,11 +36,33 @@ class ExceptionsReporter:
         exceptions: Iterable[Tuple[Type[Exception], int]],
         default_exit_code: int = DEFAULT_EXIT_CODE,
     ):
-        self.exceptions = OrderedDict(exceptions)
+        self.exceptions_items = self.sort_exceptions(exceptions)
         self.default_exit_code = default_exit_code
 
+    @staticmethod
+    def sort_exceptions(exceptions: Iterable[Tuple[Type[Exception], int]]) -> List[Tuple[Type[Exception], int]]:
+        inheritance_levels = Counter()
+        for exc, exit_code in exceptions:
+            for e, _ in exceptions:
+                if e is not exc and issubclass(exc, e):
+                    inheritance_levels[e] += 1
+        sorted_exceptions = list(exceptions)
+
+        def key(v):
+            exc, exit_code = v
+            return (inheritance_levels[exc], exit_code)
+        return sorted(sorted_exceptions, key=key)
+
+    def found_exception_item(self, exc_type: Type[Exception]):
+        for item in self.exceptions_items:
+            print(exc_type, item[0], issubclass(exc_type, item[0]))
+            if issubclass(exc_type, item[0]):
+                return item
+        return None
+
     def exception_exit_code(self, e: Exception):
-        return self.exceptions.get(e.__class__, self.default_exit_code)
+        item = self.found_exception_item(e.__class__)
+        return item[1] if item is not None else self.default_exit_code
 
     def report(
         self,
@@ -54,7 +76,7 @@ class ExceptionsReporter:
         def add_report(k: str, v: str):
             report[k] = cuts_all_non_ascii_chars(v)
 
-        if e.__class__ in self.exceptions:
+        if self.found_exception_item(e.__class__) is not None:
             if level in (ReportLevel.MESSAGE, ReportLevel.TYPE):
                 add_report("type", e.__class__.__name__)
             if level == ReportLevel.MESSAGE:
