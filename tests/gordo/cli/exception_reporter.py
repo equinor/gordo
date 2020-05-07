c204951aefeebdbb0e371dfe020cf345a223d617
Serhii Koropets <skoropets@gmail.com>
2020-05-07 10:12:18 +0300

    Found exceptions ExceptionsReporter depends on their inheritance level

--- tests/gordo/cli/exception_reporter.py
+++ tests/gordo/cli/exception_reporter.py
@@ -29,6 +29,10 @@ class _Test2Exception(Exception):
     pass
 
 
+class _Test3Exception(_Test1Exception):
+    pass
+
+
 @pytest.fixture
 def reporter1():
     return ExceptionsReporter(((_Test1Exception, 110),))
@@ -39,9 +43,16 @@ def get_result(sio):
     return json.loads(value)
 
 
+def test_sort_exceptions():
+    exceptions = ((Exception, 10), (IOError, 20), (FileNotFoundError, 30))
+    sorted_exceptions = ExceptionsReporter.sort_exceptions(exceptions)
+    assert sorted_exceptions == [(FileNotFoundError, 30), (OSError, 20), (Exception, 10)]
+
+
 def test_reporter1(reporter1):
     assert reporter1.exception_exit_code(_Test1Exception()) == 110
     assert reporter1.exception_exit_code(_Test2Exception()) == DEFAULT_EXIT_CODE
+    assert reporter1.exception_exit_code(_Test3Exception()) == 110
 
 
 def test_with_message_report_level(reporter1):
@@ -80,19 +91,31 @@ def test_with_unicode_chars(reporter1):
 
 def test_with_max_message_len(reporter1):
     report_file = StringIO()
-    reporter1.report(ReportLevel.MESSAGE, _Test1Exception("Hello world!"), report_file, max_message_len=8)
+    reporter1.report(
+        ReportLevel.MESSAGE,
+        _Test1Exception("Hello world!"),
+        report_file,
+        max_message_len=8,
+    )
     assert get_result(report_file) == {
         "type": "_Test1Exception",
         "message": "Hello...",
     }
     report_file = StringIO()
-    reporter1.report(ReportLevel.MESSAGE, _Test1Exception("Hello world!"), report_file, max_message_len=20)
+    reporter1.report(
+        ReportLevel.MESSAGE,
+        _Test1Exception("Hello world!"),
+        report_file,
+        max_message_len=20,
+    )
     assert get_result(report_file) == {
         "type": "_Test1Exception",
         "message": "Hello world!",
     }
     report_file = StringIO()
-    reporter1.report(ReportLevel.MESSAGE, _Test1Exception("Hello"), report_file, max_message_len=4)
+    reporter1.report(
+        ReportLevel.MESSAGE, _Test1Exception("Hello"), report_file, max_message_len=4
+    )
     assert get_result(report_file) == {
         "type": "_Test1Exception",
         "message": "",
