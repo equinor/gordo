7599fded4269536d516968cb5d1232abe66f75fd
Serhii Koropets <skoropets@gmail.com>
2020-05-09 18:39:04 +0300

    Use sys.exc_info() for exception result in ExceptionReporter

--- tests/gordo/cli/exception_reporter.py
+++ tests/gordo/cli/exception_reporter.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import pytest
 
@@ -38,11 +39,6 @@ def reporter1():
     return ExceptionsReporter(((_Test1Exception, 110),))
 
 
-def get_result(sio):
-    value = sio.getvalue()
-    return json.loads(value)
-
-
 def test_sort_exceptions():
     exceptions = ((Exception, 10), (IOError, 20), (FileNotFoundError, 30))
     sorted_exceptions = ExceptionsReporter.sort_exceptions(exceptions)
@@ -54,73 +50,89 @@ def test_sort_exceptions():
 
 
 def test_reporter1(reporter1):
-    assert reporter1.exception_exit_code(_Test1Exception()) == 110
-    assert reporter1.exception_exit_code(_Test2Exception()) == DEFAULT_EXIT_CODE
-    assert reporter1.exception_exit_code(_Test3Exception()) == 110
+    assert reporter1.exception_exit_code(_Test1Exception) == 110
+    assert reporter1.exception_exit_code(_Test2Exception) == DEFAULT_EXIT_CODE
+    assert reporter1.exception_exit_code(_Test3Exception) == 110
+
+
+def report(e, reporter, report_level, report_file, **kwargs):
+    try:
+        raise e
+    except Exception:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        reporter.report(
+            report_level, exc_type, exc_value, exc_traceback, report_file, **kwargs
+        )
+
+
+def report_to_string(e, reporter, report_level, **kwargs):
+    sio = StringIO()
+    report(e, reporter, report_level, sio, **kwargs)
+    value = sio.getvalue()
+    return json.loads(value)
 
 
 def test_with_message_report_level(reporter1):
-    report_file = StringIO()
-    reporter1.report(ReportLevel.MESSAGE, _Test1Exception("Test message"), report_file)
-    assert get_result(report_file) == {
+    result = report_to_string(
+        _Test1Exception("Test message"), reporter1, ReportLevel.MESSAGE
+    )
+    assert result == {
         "type": "_Test1Exception",
         "message": "Test message",
     }
 
 
 def test_with_type_report_level(reporter1):
-    report_file = StringIO()
-    reporter1.report(ReportLevel.TYPE, _Test1Exception("Test message"), report_file)
-    assert get_result(report_file) == {
+    result = report_to_string(
+        _Test1Exception("Test message"), reporter1, ReportLevel.TYPE
+    )
+    assert result == {
         "type": "_Test1Exception",
     }
 
 
 def test_with_exit_code_report_level(reporter1):
-    report_file = StringIO()
-    reporter1.report(
-        ReportLevel.EXIT_CODE, _Test1Exception("Test message"), report_file
+    result = report_to_string(
+        _Test1Exception("Test message"), reporter1, ReportLevel.EXIT_CODE
     )
-    assert get_result(report_file) == {}
+    assert result == {}
 
 
 def test_with_unicode_chars(reporter1):
-    report_file = StringIO()
-    reporter1.report(ReportLevel.MESSAGE, _Test1Exception("你好 world!"), report_file)
-    assert get_result(report_file) == {
+    result = report_to_string(
+        _Test1Exception("你好 world!"), reporter1, ReportLevel.MESSAGE
+    )
+    assert result == {
         "type": "_Test1Exception",
         "message": "?? world!",
     }
 
 
 def test_with_max_message_len(reporter1):
-    report_file = StringIO()
-    reporter1.report(
-        ReportLevel.MESSAGE,
+    result = report_to_string(
         _Test1Exception("Hello world!"),
-        report_file,
+        reporter1,
+        ReportLevel.MESSAGE,
         max_message_len=8,
     )
-    assert get_result(report_file) == {
+    assert result == {
         "type": "_Test1Exception",
         "message": "Hello...",
     }
-    report_file = StringIO()
-    reporter1.report(
-        ReportLevel.MESSAGE,
+    result = report_to_string(
         _Test1Exception("Hello world!"),
-        report_file,
+        reporter1,
+        ReportLevel.MESSAGE,
         max_message_len=20,
     )
-    assert get_result(report_file) == {
+    assert result == {
         "type": "_Test1Exception",
         "message": "Hello world!",
     }
-    report_file = StringIO()
-    reporter1.report(
-        ReportLevel.MESSAGE, _Test1Exception("Hello"), report_file, max_message_len=4
+    result = report_to_string(
+        _Test1Exception("Hello"), reporter1, ReportLevel.MESSAGE, max_message_len=4
     )
-    assert get_result(report_file) == {
+    assert result == {
         "type": "_Test1Exception",
         "message": "",
     }
