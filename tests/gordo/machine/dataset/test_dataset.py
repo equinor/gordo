8dcc6e85e843d5727c594451b5e10180baced737
henrikny <henriknyhus@gmail.com>
2020-07-16 18:27:38 +0200

    Assertions edit

--- tests/gordo/machine/dataset/test_dataset.py
+++ tests/gordo/machine/dataset/test_dataset.py
@@ -147,7 +147,7 @@ def test_join_timeseries_nonutcstart(dataset):
     all_in_frame = dataset.join_timeseries(
         timeseries_list, resampling_start, resampling_end, frequency
     )
-    assert len(all_in_frame) == 1854
+    assert len(all_in_frame) == 49
 
 
 def test_join_timeseries_with_gaps(dataset):
