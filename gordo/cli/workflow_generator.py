12fb49e047ad5df03c060786701ad32d639d5258
Serhii Koropets <skoropets@gmail.com>
2020-08-25 12:06:32 +0300

    Number of workers for Prometheus metrics servers

--- gordo/cli/workflow_generator.py
+++ gordo/cli/workflow_generator.py
@@ -168,8 +168,14 @@ def workflow_cli(gordo_ctx):
 @click.option(
     "--without-prometheus",
     is_flag=True,
-    help="Do not deploy prometheus for Gordo servers monitoring",
+    help="Do not deploy Prometheus for Gordo servers monitoring",
     envvar=f"{PREFIX}_WITHOUT_PROMETHEUS",
+)
+@click.option(
+    "--prometheus-metrics-server-workers",
+    help="Number of workers for Prometheus metrics servers",
+    envvar=f"{PREFIX}_PROMETHEUS_METRICS_SERVER_WORKERS",
+    default=1,
 )
 @click.pass_context
 def workflow_generator_cli(gordo_ctx, **ctx):
