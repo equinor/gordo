db636e5d97ad4c78f1a332cd190d49f042ca1424
henrikny <henriknyhus@gmail.com>
2020-08-26 15:54:03 +0200

    update test

--- tests/gordo/workflow/test_config_elements.py
+++ tests/gordo/workflow/test_config_elements.py
@@ -136,6 +136,7 @@ def test_machine_from_config(default_globals: dict):
                 "type": "DataLakeProvider",
             },
             "default_asset": None,
+            "filter_periods": {},
             "high_threshold": 50000,
             "interpolation_limit": "8H",
             "interpolation_method": "linear_interpolation",
