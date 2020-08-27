e895dc248683e5e0e599d45d5828d812b4b934c9
henrikny <henriknyhus@gmail.com>
2020-08-27 10:20:49 +0200

    added test

---
+++ tests/gordo/machine/dataset/test_filter_periods.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from gordo.machine.dataset.datasets import RandomDataset
+from gordo.machine.dataset.filter_periods import filter_periods
+from gordo.machine.dataset.sensor_tag import SensorTag
+
+@pytest.fixture
+def dataset():
+    return RandomDataset(
+        train_start_date="2017-01-01 00:00:00Z",
+        train_end_date="2018-01-01 00:00:00Z",
+        tag_list=[SensorTag("Tag 1", None), SensorTag("Tag 2", None)],
+    )
+
+
+def test_filter_periods(dataset):
+    data, _ = dataset.get_data()
+
+
+    with pytest.raises(TypeError):
+        filter_periods(data=data, granularity="10T", filter_method="abc", n_iqr=1)
+
+    data_filtered = filter_periods(data=data, granularity="10T", filter_method="median", n_iqr=1)
+    assert data_filtered.data.shape == (1460, 2)
+
+    data_filtered = filter_periods(data=data, granularity="10T", filter_method="iforest", iforest_smooth=False)
+    assert data_filtered.data.shape == (1775, 2)
+
+    data_filtered = filter_periods(data=data, granularity="10T", filter_method="iforest", iforest_smooth=True)
+    assert data_filtered.data.shape == (1584, 2)
+
+    data_filtered = filter_periods(data=data, granularity="10T", filter_method="all", n_iqr=1)
+    assert data_filtered.data.shape == (1436, 2)
