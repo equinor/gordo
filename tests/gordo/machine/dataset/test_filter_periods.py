79402ddf4d7a7da6035418e6b6e95345bd7f8544
henrikny <henriknyhus@gmail.com>
2020-08-27 16:38:30 +0200

    shape change

--- tests/gordo/machine/dataset/test_filter_periods.py
+++ tests/gordo/machine/dataset/test_filter_periods.py
@@ -35,4 +35,4 @@ def test_filter_periods(dataset):
     data_filtered = filter_periods(
         data=data, granularity="10T", filter_method="all", n_iqr=1, iforest_smooth=False
     )
-    assert data_filtered.data.shape == (1589, 2)
+    assert data_filtered.data.shape == (1590, 2)
