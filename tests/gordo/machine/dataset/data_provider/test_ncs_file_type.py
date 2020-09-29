aa992dc28c935034f73fd8ac565199c59be384b1
Serhii Koropets <skoropets@gmail.com>
2020-09-29 19:29:20 +0300

    test_ncs_lookup

--- tests/gordo/machine/dataset/data_provider/test_ncs_file_type.py
+++ tests/gordo/machine/dataset/data_provider/test_ncs_file_type.py
@@ -1,19 +1,23 @@
 from gordo.machine.dataset.data_provider.file_type import CsvFileType, ParquetFileType
-from gordo.machine.dataset.data_provider.ncs_file_type import NcsCsvFileType, NcsParquetFileType, load_ncs_file_types
+from gordo.machine.dataset.data_provider.ncs_file_type import (
+    NcsCsvFileType,
+    NcsParquetFileType,
+    load_ncs_file_types,
+)
 
 
 def test_ncs_csv_file_type(mock_file_system):
     ncs_file_type = NcsCsvFileType()
     assert type(ncs_file_type.file_type) is CsvFileType
     paths = ncs_file_type.paths(mock_file_system, "tag1", 2020)
-    assert list(paths) == ['tag1_2020.csv']
+    assert list(paths) == ["tag1_2020.csv"]
 
 
 def test_ncs_parquet_file_type(mock_file_system):
     ncs_file_type = NcsParquetFileType()
     assert type(ncs_file_type.file_type) is ParquetFileType
     paths = ncs_file_type.paths(mock_file_system, "tag1", 2020)
-    assert list(paths) == ['parquet/tag1_2020.parquet']
+    assert list(paths) == ["parquet/tag1_2020.parquet"]
 
 
 def test_load_ncs_file_types():
@@ -21,6 +25,6 @@ def test_load_ncs_file_types():
     assert len(ncs_file_types) == 2
     assert type(ncs_file_types[0]) is NcsParquetFileType
     assert type(ncs_file_types[1]) is NcsCsvFileType
-    ncs_file_types = load_ncs_file_types(('parquet',))
+    ncs_file_types = load_ncs_file_types(("parquet",))
     assert len(ncs_file_types) == 1
     assert type(ncs_file_types[0]) is NcsParquetFileType
---
+++ tests/gordo/machine/dataset/data_provider/test_ncs_lookup.py
@@ -0,0 +1,149 @@
+import pytest
+import posixpath
+
+from unittest.mock import MagicMock
+
+from gordo.machine.dataset.data_provider.file_type import ParquetFileType, CsvFileType
+from gordo.machine.dataset.file_system import FileType, FileInfo
+from gordo.machine.dataset.data_provider.ncs_lookup import NcsLookup, TagLocation
+from gordo.machine.dataset.sensor_tag import SensorTag
+
+from typing import List, Tuple, Dict, Optional
+
+
+@pytest.fixture
+def dir_tree():
+    return [
+        # tag.name = Ásgarðr
+        ("path/%C3%81sgar%C3%B0r", FileType.DIRECTORY),
+        ("path/%C3%81sgar%C3%B0r/%C3%81sgar%C3%B0r_2019.csv", FileType.FILE),
+        ("path/tag2", FileType.DIRECTORY),
+        ("path/tag2/parquet", FileType.DIRECTORY),
+        ("path/tag2/parquet/tag2_2020.parquet", FileType.FILE),
+        ("path/tag3", FileType.DIRECTORY),
+        ("path/tag3/parquet", FileType.DIRECTORY),
+        ("path/tag3/parquet/tag3_2020.parquet", FileType.FILE),
+        ("path/tag3/tag3_2020.csv", FileType.FILE),
+    ]
+
+
+@pytest.fixture
+def mock_file_system(dir_tree):
+    def ls_side_effect(path, with_info=True):
+        for file_path, file_type in dir_tree:
+            dir_path, _ = posixpath.split(file_path)
+            if dir_path == path:
+                yield file_path, FileInfo(file_type, 0) if with_info else None
+
+    def walk_side_effect(base_path, with_info=True):
+        for file_path, file_type in dir_tree:
+            if file_path.find(base_path) == 0:
+                yield file_path, FileInfo(file_type, 0) if with_info else None
+
+    def exists_side_effect(path):
+        for file_path, _ in dir_tree:
+            if path == file_path:
+                return True
+        return False
+
+    mock = MagicMock()
+    mock.exists.side_effect = exists_side_effect
+    mock.ls.side_effect = ls_side_effect
+    mock.walk.side_effect = walk_side_effect
+    mock.join.side_effect = posixpath.join
+    mock.split.side_effect = posixpath.split
+    return mock
+
+
+def test_mock_file_system(mock_file_system):
+    result = list(mock_file_system.ls("path"))
+    assert result == [
+        ("path/%C3%81sgar%C3%B0r", FileInfo(file_type=FileType.DIRECTORY, size=0),),
+        ("path/tag2", FileInfo(file_type=FileType.DIRECTORY, size=0),),
+        ("path/tag3", FileInfo(file_type=FileType.DIRECTORY, size=0),),
+    ]
+    result = list(mock_file_system.walk("path/tag2"))
+    assert result == [
+        ("path/tag2", FileInfo(file_type=FileType.DIRECTORY, size=0),),
+        ("path/tag2/parquet", FileInfo(file_type=FileType.DIRECTORY, size=0),),
+        (
+            "path/tag2/parquet/tag2_2020.parquet",
+            FileInfo(file_type=FileType.FILE, size=0),
+        ),
+    ]
+    assert mock_file_system.exists("path/%C3%81sgar%C3%B0r")
+    assert not mock_file_system.exists("path/out")
+
+
+@pytest.fixture
+def default_ncs_lookup(mock_file_system):
+    return NcsLookup.create(mock_file_system)
+
+
+def test_tag_dirs_lookup(default_ncs_lookup: NcsLookup):
+    tags = [
+        SensorTag("Ásgarðr", "asset"),
+        SensorTag("tag1", "asset"),
+        SensorTag("tag2", "asset"),
+        SensorTag("tag4", "asset"),
+    ]
+    result = {}
+    for tag, path in default_ncs_lookup.tag_dirs_lookup("path", tags):
+        result[tag.name] = path
+    assert result == {
+        "Ásgarðr": "path/%C3%81sgar%C3%B0r",
+        "tag2": "path/tag2",
+        "tag1": None,
+        "tag4": None,
+    }
+
+
+def reduce_tag_locations(tag_locations):
+    result = {}
+    for location in tag_locations:
+        result[(location.tag.name, location.year)] = (
+            location.path,
+            type(location.file_type) if location.file_type is not None else None,
+        )
+    return result
+
+
+def test_files_lookup_asgard(default_ncs_lookup: NcsLookup):
+    tag = SensorTag("Ásgarðr", "asset")
+    result = []
+    for location in default_ncs_lookup.files_lookup(
+        "path/%C3%81sgar%C3%B0r", tag, [2019, 2020]
+    ):
+        result.append(location)
+    assert len(result) == 2
+    assert reduce_tag_locations(result) == {
+        ("Ásgarðr", 2019): (
+            "path/%C3%81sgar%C3%B0r/%C3%81sgar%C3%B0r_2019.csv",
+            CsvFileType,
+        ),
+        ("Ásgarðr", 2020): (None, None),
+    }
+
+
+def test_files_lookup_tag2(default_ncs_lookup: NcsLookup):
+    tag = SensorTag("tag2", "asset")
+    result = []
+    for location in default_ncs_lookup.files_lookup("path/tag2", tag, [2019, 2020]):
+        result.append(location)
+    assert len(result) == 2
+    assert reduce_tag_locations(result) == {
+        ("tag2", 2020): ("path/tag2/parquet/tag2_2020.parquet", ParquetFileType),
+        ("tag2", 2019): (None, None),
+    }
+
+
+def test_files_lookup_tag3(default_ncs_lookup: NcsLookup):
+    tag = SensorTag("tag3", "asset")
+    result = []
+    for location in default_ncs_lookup.files_lookup("path/tag3", tag, [2019, 2020]):
+        result.append(location)
+    assert len(result) == 2
+    assert reduce_tag_locations(result) == {
+        ("tag3", 2020): ("path/tag3/parquet/tag3_2020.parquet", ParquetFileType),
+        ("tag3", 2019): (None, None),
+    }
