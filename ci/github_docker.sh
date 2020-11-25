a9f79a96dda34eef9433f247ffcae93f02d89e05
Serhii Koropets <skoropets@gmail.com>
2020-11-25 19:49:53 +0200

    Refactoring for master-ci.yml

---
+++ ci/github_docker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+DOCKER_DEV_IMAGE=${DOCKER_DEV_REGISTRY}/gordo
+DOCKER_PROD_IMAGE=${DOCKER_PROD_REGISTRY}/gordo
+
+if [[ $GITHUB_REF == refs/tags/* ]]; then
+    VERSION=${GITHUB_REF#refs/tags/}
+else
+    VERSION=${GITHUB_SHA::8}
+fi
+
+IMAGE_TYPE="dev"
+if [ $GITHUB_EVENT_NAME == "release" ]; then
+    prerelease=`cat "$GITHUB_EVENT_PATH" | jq -rM .release.prerelease`
+    if [ $prerelease == "false" ]; then
+        IMAGE_TYPE="prod"
+    fi
+fi
+
+function output_tags {
+    var_name=$1
+    image_name=$2
+    tags=$DOCKER_DEV_IMAGE/$image_name:$VERSION,$DOCKER_DEV_IMAGE/$image_name:latest
+    if [[ $IMAGE_TYPE == "prod" ]]; then
+        tags=$tags,$DOCKER_PROD_IMAGE/$image_name:$VERSION,$DOCKER_PROD_IMAGE/$image_name:latest
+        tags=$tags,$DOCKER_PROD_IMAGE/$image_name:stable
+    fi
+    echo ::set-output name=$var_name::$tags
+}
+
+echo ::set-output name=version::${VERSION}
+echo ::set-output name=image_type::${IMAGE_TYPE}
+echo ::set-output name=created::$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+echo ::set-output name=base_image::gordo/base:$VERSION
+output_tags "tags_gordo_client" "gordo-client"
+output_tags "tags_gordo_deploy" "gordo-deploy"
+output_tags "tags_gordo_model_builder" "gordo-model-builder"
+output_tags "tags_gordo_model_server" "gordo-model-server"
