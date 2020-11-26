8951b043703dc191a2dd591291d2437ab0e49d0a
Serhii Koropets <skoropets@gmail.com>
2020-11-26 23:04:36 +0200

    workflow for PR

--- ci/github_docker.sh
+++ ci/github_docker.sh
@@ -5,13 +5,21 @@ set -e
 DOCKER_DEV_IMAGE=${DOCKER_DEV_REGISTRY}/gordo
 DOCKER_PROD_IMAGE=${DOCKER_PROD_REGISTRY}/gordo
 
+IMAGE_TYPE="dev"
 if [[ $GITHUB_REF == refs/tags/* ]]; then
     VERSION=${GITHUB_REF#refs/tags/}
-else
+elif [[ $GITHUB_REF == refs/pull/* ]]; then
+    number=`cat "$GITHUB_EVENT_PATH" | jq -rM .number`
+    if [ -n "$number" ]; then
+        VERSION=pr-$number
+        IMAGE_TYPE="pr"
+    fi
+fi
+
+if [ -z "$VERSION" ]; then
     VERSION=${GITHUB_SHA::8}
 fi
 
-IMAGE_TYPE="dev"
 STABLE=""
 if [ "$GITHUB_EVENT_NAME" == "release" ]; then
     IMAGE_TYPE="prod"
@@ -39,14 +47,18 @@ function version_tags {
 function set_output_tags {
     var_name=$1
     image_name=$2
-    tags=$DOCKER_DEV_IMAGE/$image_name:latest,$(version_tags "$DOCKER_DEV_IMAGE/$image_name" "$VERSION")
-    if [ "$IMAGE_TYPE" == "prod" ]; then
-        tags=$tags,$DOCKER_PROD_IMAGE/$image_name:latest,$(version_tags "$DOCKER_PROD_IMAGE/$image_name" "$VERSION")
-    fi
-    if [ "$STABLE" == "true" ]; then
-        tags=$tags,$DOCKER_DEV_IMAGE/$image_name:stable
+    if [ "$IMAGE_TYPE" == "pr" ]; then
+        tags=$DOCKER_DEV_IMAGE/$image_name:$VERSION
+    else
+        tags=$DOCKER_DEV_IMAGE/$image_name:latest,$(version_tags "$DOCKER_DEV_IMAGE/$image_name" "$VERSION")
         if [ "$IMAGE_TYPE" == "prod" ]; then
-            tags=$tags,$DOCKER_PROD_IMAGE/$image_name:stable
+            tags=$tags,$DOCKER_PROD_IMAGE/$image_name:latest,$(version_tags "$DOCKER_PROD_IMAGE/$image_name" "$VERSION")
+        fi
+        if [ "$STABLE" == "true" ]; then
+            tags=$tags,$DOCKER_DEV_IMAGE/$image_name:stable
+            if [ "$IMAGE_TYPE" == "prod" ]; then
+                tags=$tags,$DOCKER_PROD_IMAGE/$image_name:stable
+            fi
         fi
     fi
     echo ::set-output name=$var_name::$tags
