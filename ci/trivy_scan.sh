4e5d224b80b44bd6be57ba5a517f4629b00b4a68
Serhii Koropets <skoropets@gmail.com>
2020-11-26 16:34:18 +0200

    Update trivy scanner

--- ci/trivy_scan.sh
+++ ci/trivy_scan.sh
@@ -11,29 +11,21 @@ fi
 uname=$(uname -s 2>/dev/null)
 trivy=$(which trivy);
 if [ -z "$trivy" ]; then
-    if [ "$uname" == "Darwin" ]; then
-        machine="macOS"
-    elif [ "$uname"  == "Linux" ]; then
-        machine="Linux"
+    if [ "$uname"  == "Linux" ]; then
+        sudo apt-get install wget apt-transport-https gnupg lsb-release
+        wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+        echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+        sudo apt-get update
+        sudo apt-get install trivy
     else
         echo "Unable to determine platform '$uname'" 
         exit 1
     fi
-    TRIVY_VERSION=$(curl --silent "https://api.github.com/repos/aquasecurity/trivy/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/');
-    echo "Downloading trivy..";
-    if [ -n "$TRIVY_VERSION" ] && [ -n "$machine" ]; then
-        curl -Ls "https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_${machine}-64bit.tar.gz" 
-        if tar zx --wildcards '*trivy'; then 
-            echo "Download or extract failed for '${machine}' version '${TRIVY_VERSION}'."
-            exit 1
-        fi
-        trivy="./trivy";
-    fi
-else
-    TRIVY_VERSION=$(trivy -v 2>/dev/null | head -1 | cut -d ' ' -f 2);
 fi
 
-echo "Trivy version is ${TRIVY_VERSION} and platform is ${uname}"
+trivy_version=$(trivy -v 2>/dev/null | head -1 | cut -d ' ' -f 2);
+
+echo "Trivy version is ${trivy_version} and platform is ${uname}"
 echo "Scanning image - '$image'"
 
 $trivy --clear-cache 
