8cbaaadd6e3b35aef65cfea236eb958b8729578c
Serhii Koropets <skoropets@gmail.com>
2020-11-27 11:29:14 +0200

    Fixing github_docker.sh

--- ci/github_docker.sh
+++ ci/github_docker.sh
@@ -20,12 +20,13 @@ if [ -z "$VERSION" ]; then
     VERSION=${GITHUB_SHA::8}
 fi
 
-STABLE=""
+RELEASE=""
 if [ "$GITHUB_EVENT_NAME" == "release" ]; then
+    RELEASE="prerelease"
     IMAGE_TYPE="prod"
     prerelease=`cat "$GITHUB_EVENT_PATH" | jq -rM .release.prerelease`
     if [ "$prerelease" == "false" ]; then
-        STABLE="true"
+        RELEASE="release"
     fi
 fi
 
@@ -34,12 +35,8 @@ function version_tags {
     version=(${2//./ })
     output=$image:$2
     if [ ${#version[@]} -ge 3 ]; then
-        if [ -n "${version[0]}" ]; then
-            output=$output,$image:${version[0]}
-            if [ -n "${version[1]}" ]; then
-                output=$output,$image:${version[0]}.${version[1]}
-            fi
-        fi
+        output=$output,$image:${version[0]}
+        output=$output,$image:${version[0]}.${version[1]}
     fi
     echo $output
 }
@@ -50,11 +47,14 @@ function set_output_tags {
     if [ "$IMAGE_TYPE" == "pr" ]; then
         tags=$DOCKER_DEV_IMAGE/$image_name:$VERSION
     else
-        tags=$DOCKER_DEV_IMAGE/$image_name:latest,$(version_tags "$DOCKER_DEV_IMAGE/$image_name" "$VERSION")
+        tags=$(version_tags "$DOCKER_DEV_IMAGE/$image_name" "$VERSION")
+        if [ -n "$RELEASE" ]; then
+            tags=$tags,$DOCKER_DEV_IMAGE/$image_name:latest
+        fi
         if [ "$IMAGE_TYPE" == "prod" ]; then
             tags=$tags,$DOCKER_PROD_IMAGE/$image_name:latest,$(version_tags "$DOCKER_PROD_IMAGE/$image_name" "$VERSION")
         fi
-        if [ "$STABLE" == "true" ]; then
+        if [ "$RELEASE" == "release" ]; then
             tags=$tags,$DOCKER_DEV_IMAGE/$image_name:stable
             if [ "$IMAGE_TYPE" == "prod" ]; then
                 tags=$tags,$DOCKER_PROD_IMAGE/$image_name:stable
@@ -70,7 +70,7 @@ if [ "$IMAGE_TYPE" == "prod" ]; then
 fi
 
 echo ::set-output name=version::${VERSION}
-echo ::set-output name=stable::${STABLE}
+echo ::set-output name=release_type::${RELEASE}
 echo ::set-output name=image_type::${IMAGE_TYPE}
 echo ::set-output name=created::$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 echo ::set-output name=base_image::$BASE_IMAGE:$VERSION
