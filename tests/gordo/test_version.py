d36647c5491eedd67a32c53eabbb1c6652e7666c
Serhii Koropets <33310880+koropets@users.noreply.github.com>
2021-03-04 18:28:23 +0200

    Taking into account version development status for cache_key (#1136)
    * Parsion version
    
    * cache_key with IS_UNSTABLE_VERSION
    
    * Fix for mypy
    
    * Black reformatting
    
    * Fixing code coverage

--- tests/gordo/test_version.py
+++ tests/gordo/test_version.py
@@ -8,19 +8,25 @@ from gordo import _parse_version, __version__
 
 def test_version():
     assert isinstance(__version__, str)
-    versions: (int, int, int) = _parse_version(__version__)
-    for v in versions:
-        assert isinstance(v, int)
+    major, minor, is_unstable = _parse_version(__version__)
+    assert isinstance(major, int)
+    assert isinstance(minor, int)
+    assert isinstance(is_unstable, bool)
 
 
 @pytest.mark.parametrize(
     "version,expected",
     [
-        ("1.1.1", (1, 1)),
-        ("1.1.1.dev-a1", (1, 1)),
-        ("0.55.25.02", (0, 55)),
-        ("0.0.0", (0, 0)),
+        ("1.1.1", (1, 1, False)),
+        ("1.1.1.dev-a1", (1, 1, True)),
+        ("0.55.0-rc1", (0, 55, True)),
+        ("0.0.0", (0, 0, False)),
     ],
 )
 def test_version_parser(version: str, expected: Tuple[int, int, int]):
     assert _parse_version(version) == expected
+
+
+def test_version_with_error():
+    with pytest.raises(ValueError):
+        _parse_version("not_a_version")
