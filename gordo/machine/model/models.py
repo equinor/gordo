6e5df76f18e840debd28f3f55b57c01275324766
devProdigy <cherednyk.o@gmail.com>
2022-01-12 16:41:15 +0100

    black

--- gordo/machine/model/models.py
+++ gordo/machine/model/models.py
@@ -556,9 +556,9 @@ class KerasLSTMBaseEstimator(KerasBaseEstimator, TransformerMixin, metaclass=ABC
             )
         return X
 
-    def fit(
+    def fit(  # type: ignore
         self, X: np.ndarray, y: np.ndarray, **kwargs
-    ) -> "KerasLSTMForecast":  # type: ignore
+    ) -> "KerasLSTMForecast":
 
         """
         This fits a one step forecast LSTM architecture.
