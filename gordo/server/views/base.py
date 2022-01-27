f9108b7bc49ac038d228e10d2fc2e5f71fa6fb3f
Serhii Koropets <skoropets@gmail.com>
2022-01-27 16:46:29 +0200

    Black reformating

--- gordo/server/views/base.py
+++ gordo/server/views/base.py
@@ -16,7 +16,12 @@ from gordo.server.rest_api import Api
 from gordo.server import utils as server_utils
 from gordo.machine.model import utils as model_utils
 from gordo_dataset.sensor_tag import SensorTag
-from gordo.server.utils import find_path_in_dict, validate_gordo_name, validate_revision, delete_revision
+from gordo.server.utils import (
+    find_path_in_dict,
+    validate_gordo_name,
+    validate_revision,
+    delete_revision,
+)
 from gordo.utils import normalize_sensor_tags
 from gordo.server import model_io
 
