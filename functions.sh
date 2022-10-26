1250d3c1cc8e0f739b1b4f5fafe93e0d8f47c060
Serhii Koropets <skoropets@gmail.com>
2022-10-26 20:14:16 +0300

    get_argo_binary

--- functions.sh
+++ functions.sh
@@ -12,6 +12,32 @@ function store_config {
     fi
 }
 
+function get_argo_binary {
+    argo_binary="argo"
+    if [ -n "$ARGO_VERSION_NUMBER" ]; then
+        if [ -z "$ARGO_VERSIONS" ]; then
+            echo "ARGO_VERSION env var is empty" 2>&1
+            exit 1
+        fi
+        for number in `echo $ARGO_VERSIONS | jq -rM .[].number` 
+        do
+            if [ "$number" = "null" ]; then
+                number=""
+            fi
+            if [ "$number" = "$ARGO_VERSION_NUMBER"]; then
+                found="true"
+                break
+            fi
+        done
+        if [ -z "$found" ]; then
+            echo "Unable to find number ($ARGO_VERSION_NUMBER) in '$ARGO_VERSIONS'" 2>&1
+            exit 1
+        fi
+        argo_binary="argo$number"
+    fi
+    echo $argo_binary
+}
+
 function argo_submit { 
     generated_config_path="$1"
     argo lint "$generated_config_path"
