6feaa3a7eed1709779954bbfe503bb80bc5a9621
Serhii Koropets <skoropets@gmail.com>
2023-04-08 10:50:23 +0300

    sudo

--- debug_call.sh
+++ debug_call.sh
@@ -6,7 +6,7 @@ child_pid=$!
 while ps | grep $child_pid ; do
   ps aux | grep $child_pid
   pstree -sp $child_pid
-  timeout 1 strace -p $child_pid
+  sudo timeout 1 strace -p $child_pid
   ps aux | grep python
   sleep 30
 done
