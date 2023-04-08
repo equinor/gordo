282c5c9db76b9f479dd8f16512addff147c1fde4
Serhii Koropets <skoropets@gmail.com>
2023-04-08 10:57:32 +0300

    Refactor debug_call.sh

--- debug_call.sh
+++ debug_call.sh
@@ -4,10 +4,14 @@ echo "Start"
 "$@" &
 child_pid=$!
 while ps | grep $child_pid ; do
-  ps aux | grep $child_pid
-  pstree -sp $child_pid
-  sudo timeout 1 strace -p $child_pid
-  ps aux | grep python
+  if ps aux | grep $child_pid | grep Sl; then
+      echo "ps aux | grep $child_pid"
+      ps aux | grep $child_pid
+      echo "pstree -sp $child_pid"
+      pstree -sp $child_pid
+      echo "timeout 1 strace -p $child_pid"
+      sudo timeout 1 strace -p $child_pid
+  fi
   sleep 30
 done
 echo "End"
