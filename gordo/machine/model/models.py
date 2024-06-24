d6faadcf40b05be9ec5cb7988a5528a3828db6bb
RollerKnobster <rivan@equinor.com>
2024-06-24 14:51:21 +0300

    Fix: rename `build_fn` to `model`

--- gordo/machine/model/models.py
+++ gordo/machine/model/models.py
@@ -302,7 +302,7 @@ class KerasBaseEstimator(BaseWrapper, GordoBase, BaseEstimator):
             Parameters used in this estimator
         """
         params = super().get_params(**params)
-        params.pop("build_fn", None)
+        params.pop("model", None)
         params.update({"kind": self.kind})
         params.update(self.kwargs)
         return params
