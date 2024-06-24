3f2a31362c19a314cf6b042f1e41cf0b8784097b
RollerKnobster <rivan@equinor.com>
2024-06-25 00:32:26 +0300

    Fix: refactor `getattr(keras.optimizers, optimizer)` to `keras.optimizers.get(optimizer)` in autoencoders

--- gordo/machine/model/factories/feedforward_autoencoder.py
+++ gordo/machine/model/factories/feedforward_autoencoder.py
@@ -5,7 +5,7 @@ from typing import Tuple, Dict, Any, Union
 from tensorflow.keras.optimizers import Optimizer
 from tensorflow.keras import regularizers
 from tensorflow.keras.layers import Dense
-from tensorflow.python import keras
+from tensorflow import keras
 
 from tensorflow.keras.models import Sequential as KerasSequential
 from gordo.machine.model.register import register_model_builder
@@ -88,8 +88,7 @@ def feedforward_model(
 
     # Instantiate optimizer with kwargs
     if isinstance(optimizer, str):
-        Optim = getattr(keras.optimizer_v1, optimizer)
-        optimizer = Optim(**optimizer_kwargs)
+        optimizer = keras.optimizers.get({"class_name": optimizer, "config": optimizer_kwargs})
 
     # Final output layer
     model.add(Dense(n_features_out, activation=out_func))
--- gordo/machine/model/factories/lstm_autoencoder.py
+++ gordo/machine/model/factories/lstm_autoencoder.py
@@ -6,7 +6,7 @@ import tensorflow
 from tensorflow.keras.optimizers import Optimizer
 from tensorflow.keras.layers import Dense, LSTM
 from tensorflow.keras.models import Sequential as KerasSequential
-from tensorflow.python import keras
+from tensorflow import keras
 
 from gordo.machine.model.register import register_model_builder
 from gordo.machine.model.factories.utils import hourglass_calc_dims, check_dim_func_len
@@ -90,8 +90,7 @@ def lstm_model(
 
     # output layer
     if isinstance(optimizer, str):
-        Optim = getattr(keras.optimizer_v1, optimizer)
-        optimizer = Optim(**optimizer_kwargs)
+        optimizer = keras.optimizers.get({"class_name": optimizer, "config": optimizer_kwargs})
 
     model.add(Dense(units=n_features_out, activation=out_func))
 
